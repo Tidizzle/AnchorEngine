da39ce1ecad5fdbe5c6e85ecbb6c95cf362f5e04
Zach Taylor <zach@planetjester.com>
2016-12-24 22:44:42 -0500

    Fixed movement in test, fixed bugs in input

---
+++ Engine/Engine/Engine/AncInput.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Input;
+
+namespace Engine
+ {
+     public static class AncInput
+     {
+         public static KeyboardState KeyboardState;
+         public static KeyboardState PreviousKeyboardState;
+         public static MouseState MouseState;
+         public static MouseState PreviousMouseState;
+
+         public static void Update()
+         {
+             PreviousKeyboardState = KeyboardState;
+             KeyboardState = Keyboard.GetState();
+
+             PreviousMouseState = MouseState;
+             MouseState = Mouse.GetState();
+         }
+
+         public static bool KeyUp(Keys K)
+         {
+             return KeyboardState.IsKeyUp(K) && PreviousKeyboardState.IsKeyDown(K);
+         }
+
+         public static bool KeyDown(Keys K)
+         {
+             return KeyboardState.IsKeyDown(K) && PreviousKeyboardState.IsKeyUp(K);
+         }
+
+         public static bool KeyHeld(Keys K)
+         {
+             return KeyboardState.IsKeyDown(K) && PreviousKeyboardState.IsKeyDown(K);
+         }
+     }
+ }
---
+++ Engine/Engine/Engine/AncSceneController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Engine
+{
+    public class AncSceneController : IDisposable
+    {
+        internal AncSystem SYSTEM;
+        private Dictionary<string, AncScene> sceneList = new Dictionary<string, AncScene>();
+        private AncScene currentScene;
+
+        public void Add(AncScene scene)
+        {
+            sceneList.Add(scene.Name, scene);
+        }
+
+        public void Instantiate()
+        {
+            foreach (var scene in sceneList.Values)
+            {
+                scene.Instantiate(SYSTEM);
+            }
+
+            currentScene = sceneList.FirstOrDefault().Value;
+        }
+
+        public void LoadContent()
+        {
+            foreach (var scene in sceneList.Values)
+            {
+                scene.LoadContent();
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            currentScene.Update(gameTime);
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            SYSTEM.GraphicsDevice.Clear(Color.Green);
+            currentScene.Draw(gameTime);
+        }
+
+        public void Dispose()
+        {
+            currentScene.Dispose();
+        }
+
+        public void DisposeAll()
+        {
+            foreach (var scene in sceneList.Values)
+            {
+                scene.Dispose();
+            }
+        }
+
+        #region Scene Modification
+
+        internal AncScene curr;
+
+        public void switchScene(String SceneName, SpriteBatch batch)
+        {
+            curr = new DefaultScene("SafeDefault");
+            if (sceneList.ContainsKey(SceneName))
+            {
+                this.curr = sceneList[SceneName];
+            }
+
+            Console.WriteLine(curr.Name);
+            currentScene = curr;
+        }
+
+        #endregion
+    }
+}
